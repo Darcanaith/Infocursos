0fe65a2670dbdd7afcd8a1ae759d7c17c4e41730
Marcos Higueras Vega <marcoshiguerasvega@gmail.com>
2022-04-13 10:07:07 +0200

    Añadida clase Conection CNX

---
+++ Infocursos/Infocursos/CNX.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Infocursos
+{
+    public class CNX
+    {
+        public SqlConnection Connection { get; set; } = null;
+
+        public CNX()
+        {
+            Connection = new SqlConnection("Data Source=217.71.207.123,54321;Initial Catalog=INFOCURSOS_DB;" +
+                "Persist Security Info=True;User ID=sa;Password=123456789");
+            Connection.Open();
+        }
+    }
+}
